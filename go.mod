ef959cf765eaf74a16876c819396e7d73c01cd2b
SvilexArch <garmanow.kirill@yandex.ru>
2022-09-24 22:57:46 +0300

    feat(server): add proxy server implementation

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/Big-Kotik/ivt-pull
+
+go 1.18
+
+replace github.com/Big-Kotik/ivt-pull-api => ./ivt-pull-api
+
+require (
+	github.com/Big-Kotik/ivt-pull-api v0.0.0-00010101000000-000000000000
+	google.golang.org/grpc v1.49.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20220923203811-8be639271d50 // indirect
+	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220923205249-dd2d53f1fffc // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ pkg/server/server.go
@@ -0,0 +1,84 @@
+package server
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Big-Kotik/ivt-pull-api/pkg/api"
+)
+
+type PullServer struct {
+	http.Client
+	logger *log.Logger
+	api.UnimplementedPullerServer
+}
+
+// TODO: resources
+func (p *PullServer) PullResource(req *api.HttpRequestsWrapper, respStream api.Puller_PullResourceServer) error {
+	for _, req := range req.GetRequests() {
+		resp, err := p.sendRequset(req)
+
+		// TODO: как понять какой запрос дошел, а какой нет
+		if err != nil {
+			p.logger.Print(fmt.Printf("can't send request in PullResource - %s", err.Error()))
+			continue
+		}
+
+		err = respStream.Send(resp)
+
+		if err != nil {
+			p.logger.Print(fmt.Printf("can't write resp to stream - %s", err.Error()))
+		}
+	}
+
+	return nil
+}
+
+func (p *PullServer) mustEmbedUnimplementedPullerServer() {
+
+}
+
+func (p *PullServer) sendRequset(r *api.HttpRequestsWrapper_Request) (*api.Response, error) {
+	// TODO: too much logic in one place
+	req, err := http.NewRequest(r.GetMethod(), r.GetUrl(), strings.NewReader(r.GetBody()))
+
+	if err != nil {
+		p.logger.Print(fmt.Printf("can' create req in sendRequest, method: %s url: %s",
+			r.GetMethod(), r.GetUrl()))
+		return nil, err
+	}
+
+	for k, v := range r.GetHeaders() {
+		for _, key := range v.Keys {
+			req.Header.Set(k, key)
+		}
+	}
+
+	resp, err := p.Client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return respToGrpcResponse(resp)
+}
+
+func respToGrpcResponse(resp *http.Response) (*api.Response, error) {
+	grpcResp := api.Response{}
+	grpcResp.StatusCode = int32(resp.StatusCode)
+	grpcResp.ProtoMajor = int32(resp.ProtoMajor)
+	grpcResp.ProtoMinor = int32(resp.ProtoMinor)
+
+	for k, v := range resp.Header {
+		grpcResp.Header[k] = &api.Header{Keys: v}
+	}
+
+	// TODO: send by parts?
+	body, err := io.ReadAll(resp.Body)
+	grpcResp.Body = body
+
+	return &grpcResp, err
+}
